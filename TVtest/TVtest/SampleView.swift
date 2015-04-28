fd406cc2e1e9178babf1d01c1c6daf75d5ca44bf
csny <masaono_13@msn.com>
2015-04-28 19:28:07 +0900

    アップロード

---
+++ TVtest/TVtest/SampleView.swift
@@ -0,0 +1,128 @@
+//
+//  SampleView.swift
+//  TVtest
+//
+//  Created by macbook on 2015/04/27.
+//  Copyright (c) 2015年 macbook. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class SampleView: UIView {
+    // 方眼のタッチ有無を格納する配列
+    var isGreenArr = [[Bool]](count: 8, repeatedValue: [Bool](count: 6, repeatedValue: false))
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        // こっちは呼ばれないよ
+        self.backgroundColor = UIColor.clearColor()
+        println("initNSCoder")
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.clearColor()
+    }
+    
+    override func drawRect(rect: CGRect) {
+        // 描画管理の構造体contextを初期化
+        let context: CGContextRef = UIGraphicsGetCurrentContext()
+        
+        // 色を定義
+        let red: [CGFloat] = [1.0, 0.0, 0.0, 1.0]
+        let blue: [CGFloat] = [0.0, 0.0, 1.0, 1.0]
+        let cyan: [CGFloat] = [0.5, 0.8, 1.0, 1.0]
+        let green: [CGFloat] = [0.0, 1.0, 0.0, 1.0]
+        let white: [CGFloat] = [1.0, 1.0, 1.0, 0.5]
+        let yellow: [CGFloat] = [1.0, 1.0, 0.0, 0.5]
+        
+        // グリッドを描画
+        CGContextSetStrokeColor(context, cyan)
+        CGContextBeginPath(context)
+        CGContextSetLineWidth(context, 0.5)
+        for (var y: Int = 50; y < Int(self.bounds.size.height); y+=50) {
+            CGContextMoveToPoint(context, 0, CGFloat(y))
+            CGContextAddLineToPoint(context, self.bounds.size.width, CGFloat(y))
+        }
+        for (var x: Int = 50; x < Int(self.bounds.size.width); x+=50) {
+            CGContextMoveToPoint(context, CGFloat(x), 0)
+            CGContextAddLineToPoint(context, CGFloat(x), self.bounds.size.height)
+        }
+        CGContextStrokePath(context)
+        
+        // フラグに応じて矩形を描画、イベントで一部再読み込みされる
+        for(var i: Int=0;i<8;i++){
+            for(var j: Int=0;j<6;j++){
+                if(isGreenArr[i][j]){
+                    CGContextSetFillColor(context, yellow)
+                    CGContextFillRect(context, CGRectMake(CGFloat(i)*50,CGFloat(j)*50,50,50))
+                }else{
+                    CGContextSetFillColor(context, white);
+                    CGContextFillRect(context, CGRectMake(CGFloat(i)*50,CGFloat(j)*50,50,50))
+                }
+            }
+        }
+        
+        // 矩形を描画
+        CGContextSetFillColor(context, green)
+        CGContextFillRect(context, CGRectMake(50, 400, 50, 50))
+        
+        // 円を描画
+        CGContextSetFillColor(context, blue)
+        CGContextFillEllipseInRect(context, CGRectMake(250, 400, 50, 100))
+        
+        // 多角形を描画
+        CGContextSetFillColor(context, red)
+        CGContextBeginPath(context)
+        CGContextMoveToPoint(context, 150, 450)
+        CGContextAddLineToPoint(context, 200, 500)
+        CGContextAddLineToPoint(context, 200, 400)
+        CGContextFillPath(context)
+        
+        // 文字表示定義
+        let myLabel: UILabel = UILabel(frame: CGRectMake(50,300,250,50))
+        myLabel.text = "上はタッチで動作、下は固定画像"
+        myLabel.font = UIFont(name:"AvenirNext-Italic",size:14)
+        myLabel.textColor = UIColor.redColor()
+        myLabel.backgroundColor = UIColor.cyanColor();
+        // Viewにラベルを追加
+        self.addSubview(myLabel)
+    }
+    
+    // タッチイベント発生時の処理
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        // タッチ位置を点で取得
+        let touch: UITouch = touches.first as! UITouch
+        var pos: CGPoint = touch.locationInView(self)
+        
+        // タッチ位置と方眼位置を比較
+        for(var i: Int=0;i<8;i++){
+            for(var j: Int=0;j<6;j++){
+                if CGRectContainsPoint(CGRectMake(CGFloat(i)*50,CGFloat(j)*50,50,50), pos) {
+                    if(isGreenArr[i][j]){
+                        isGreenArr[i][j]=false
+                    }else{
+                        isGreenArr[i][j]=true
+                    }
+                }
+            }
+        }
+        // 適当に範囲指定して画面上半分を再描画
+        //self.setNeedsDisplay()
+        self.setNeedsDisplayInRect(CGRectMake(self.bounds.origin.x,self.bounds.origin.y,self.bounds.size.width,300))
+    }
+    
+/* その他のタッチ動作もあるらしい
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        println("touchesEnded")
+    }
+    override func touchesCancelled(touches: Set<NSObject>, withEvent event: UIEvent) {
+        println("touchesCancelled")
+    }
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        println("touchesMoved")
+    super.touchesMoved(touches , withEvent:event)
+        
+    }
+*/
+}
---
+++ TVtest/TVtest/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  TVtest
+//
+//  Created by macbook on 2015/04/27.
+//  Copyright (c) 2015年 macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // ViewController
+        self.view.backgroundColor = UIColor.whiteColor()
+        initVIEWS()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func initVIEWS() {
+        // 自前UIViewの定義
+        // UIViewController.viewの座標取得
+        var x:CGFloat = self.view.bounds.origin.x
+        var y:CGFloat = self.view.bounds.origin.y
+        // UIViewController.viewの幅と高さを取得
+        var width:CGFloat = self.view.bounds.width
+        var height:CGFloat = self.view.bounds.height
+        // 上記より画面ぴったりサイズのフレームを生成する
+        var frame:CGRect = CGRect(x: x, y: y, width: width, height: height)
+        // カスタマイズViewを生成
+        var myView:SampleView = SampleView(frame: frame)
+        myView.tag = 5000
+        
+        // ボタンの定義
+        let resetBtn: UIButton = UIButton()
+        // タグを設定
+        resetBtn.tag = 5001
+        // サイズを設定する.
+        resetBtn.frame = CGRectMake(160,270,50,30)
+        // 背景色を設定する.
+        resetBtn.backgroundColor = UIColor.blackColor()
+        // 枠を丸くする.
+        //resetBtn.layer.masksToBounds = true
+        // タイトルを設定
+        resetBtn.setTitle("Reset", forState: UIControlState.Normal)
+        resetBtn.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        // コーナーの半径を設定する.
+        //resetBtn.layer.cornerRadius = 20.0
+        // ボタンの位置を指定する.
+        //resetBtn.layer.position = CGPoint(x: self.view.frame.width/2, y:200)
+        // イベントを追加する.
+        resetBtn.addTarget(self, action: "button_Tapped:", forControlEvents: .TouchUpInside)
+        
+        
+        // カスタマイズViewを追加
+        self.view.addSubview(myView)
+        // カスタマイズviewを最前面へ
+        // self.view.bringSubviewToFront(myView)
+        // ボタンをViewに追加する.
+        self.view.addSubview(resetBtn)
+    }
+    
+    // ボタン動作
+    func button_Tapped(sender: UIButton){
+        
+        // view削除
+        let beforeView1: UIView = self.view.viewWithTag(5000)!
+        let beforeView2: UIView = self.view.viewWithTag(5001)!
+        beforeView1.removeFromSuperview()
+        beforeView2.removeFromSuperview()
+        /* これでも可
+        for view in self.view.subviews {
+            view.removeFromSuperview()
+        }
+        */
+        
+        // 再作成
+        initVIEWS()
+    }
+}
+
